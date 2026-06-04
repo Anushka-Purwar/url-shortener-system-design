fb84b53bd4d171ee7c020729d31e96a74a845587
Anushka Purwar <anushkapurwar799@gmail.com>
2026-06-04 23:15:10 +0530

    featadding redis cahce and comments

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import app from "./app.js";
+import { redis } from "./config/redis.js";
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+    await redis.connect();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+  }
+
+startServer();
--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -1,9 +1,17 @@
 import {prisma} from "../config/prisma";
 import { encodebase62 } from "../utils/base62";
 import { ConflictError } from "../errors/url.errors";
+import { redis } from "../config/redis";
 
 export class UrlService{
+    /*--------helper functions------------------*/
+    async invalidateCache(shortCode: string) {
+        await redis.del(shortCode);
+    }
 
+    /*--------service functions------------------*/
+
+    //create url service
     async createUrl(originalUrl : string, customAlias? :string, expiresAt?: string){
         
         if(customAlias){
@@ -42,7 +50,18 @@ export class UrlService{
         return updateUrl;
     }
 
+    //redirect logic to get link from shortCode
     async redirect(shortCode : string) {
+
+        const cache = await redis.get(shortCode)
+
+        if (cache) {
+            console.log("CACHE HIT");
+            return JSON.parse(cache);
+        }
+        console.log("CACHE Miss");
+
+
         const url = await prisma.url.findUnique({
         where: {
             shortCode,
@@ -52,13 +71,22 @@ export class UrlService{
         if (!url) {
             return null;
         }
-        return prisma.url.findUnique({
-            where:{
-                shortCode
+
+        await redis.set(
+            shortCode,
+            JSON.stringify({
+                ...url,
+                id: url.id.toString()
+            }),
+            {
+                EX: 3600
             }
-        })
+        );
+
+       return url
     }
 
+    //increment clicks based on how many hits user made in db
     async incrementClicks(shortCode : string){
         const url = await prisma.url.findUnique({
         where: {
@@ -83,6 +111,7 @@ export class UrlService{
         })
     }
 
+    //service to get status of urls
     async stats(shortCode : string){
         const url = await prisma.url.findUnique({
         where: {
@@ -114,6 +143,8 @@ export class UrlService{
             return null;
         }
 
+        await this.invalidateCache(shortCode);
+
         return prisma.url.update({
             where:{
                 shortCode
@@ -136,6 +167,8 @@ export class UrlService{
             return null;
         }
 
+        await this.invalidateCache(shortCode);
+
         return prisma.url.update({
             where:{
                 shortCode
@@ -157,6 +190,8 @@ export class UrlService{
         if (!url) {
             return null;
         }
+
+        await this.invalidateCache(shortCode);
 
         return prisma.url.update({
             where:{
